5f27cc3e29f3e995e737ea4b76e76242235e9d84
anancds <anancds@163.com>
2016-11-25 10:23:02 +0800

    add fair-scheduler

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Spark的一些资源
 
-##课程
+## 课程
 
-##书籍
+## 书籍
 
 [Mastering Apache Spark](https://www.gitbook.com/book/jaceklaskowski/mastering-apache-spark/details "Mastering Apache Spark")
 
-##网络资源
+## 网络资源
 [Spark on yarn的内存分配问题](http://www.voidcn.com/blog/wisgood/article/p-5974273.html)
 
-##Spark summit
+## Spark summit
 
 [spark summit](https://spark-summit.org/)
+
+## spark启动参数
+
+    /bigdata/salut/components/spark/bin/spark-submit --class com.uniview.salut.streaming.DealStreaming --name "StreamingProcessing" --master yarn-cluster --driver-memory 4g --num-executors ${NUM_EXECUTORS} --executor-memory 2g --executor-cores 6 --queue root.streaming --conf spark.streaming.concurrentJobs=3 --conf spark.yarn.executor.memoryOverhead=2g --conf spark.yarn.driver.memoryOverhead=2g /bigdata/salut/lib/salut-streaming*.jar ${IPS} ${IS_CLUSTER}
---
+++ fair-scheduler/cluster/hadoop-env.sh
@@ -0,0 +1,78 @@
+# The java implementation to use.
+export JAVA_HOME=/usr/jdk/jdk1.8.0_45
+export HADOOP_PREFIX=/bigdata/salut/components/hadoop
+export HADOOP_HOME=/bigdata/salut/components/hadoop
+# The jsvc implementation to use. Jsvc is required to run secure datanodes.
+#export JSVC_HOME=${JSVC_HOME}
+
+export HADOOP_CONF_DIR=${HADOOP_CONF_DIR:-"/etc/hadoop"}
+
+# Extra Java CLASSPATH elements.  Automatically insert capacity-scheduler.
+for f in $HADOOP_HOME/contrib/capacity-scheduler/*.jar; do
+  if [ "$HADOOP_CLASSPATH" ]; then
+    export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$f
+  else
+    export HADOOP_CLASSPATH=$f
+  fi
+done
+
+for f in /bigdata/salut/lib/*.jar; do
+  if [ "$HADOOP_CLASSPATH" ]; then
+    export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$f
+  else
+    export HADOOP_CLASSPATH=$f
+  fi
+done
+
+for f in $HBASE_HOME/lib/*.jar; do  
+  flag=`echo "$f" | grep "slf4j-log4j12"`
+  if [ ! -z "$flag" ]; then
+    continue
+  fi
+  if [ "$HADOOP_CLASSPATH" ]; then    
+    export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:$f  
+  else   
+    export HADOOP_CLASSPATH=$f  
+  fi
+done
+
+# The maximum amount of heap to use, in MB. Default is 1000.
+export HADOOP_HEAPSIZE=12000
+#export HADOOP_NAMENODE_INIT_HEAPSIZE=""
+
+# Extra Java runtime options.  Empty by default.
+export HADOOP_OPTS=-server
+export HADOOP_OPTS="$HADOOP_OPTS -Djava.net.preferIPv4Stack=true"
+
+# Command specific options appended to HADOOP_OPTS when specified
+export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_NAMENODE_OPTS"
+export HADOOP_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_SECONDARYNAMENODE_OPTS"
+export HADOOP_DATANODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_DATANODE_OPTS"
+export HADOOP_BALANCER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_BALANCER_OPTS"
+export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER_OPTS"
+# export HADOOP_TASKTRACKER_OPTS=
+
+export HADOOP_NAMENODE_OPTS="-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode ${HADOOP_NAMENODE_OPTS}"
+export HADOOP_DATANODE_OPTS="-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode ${HADOOP_DATANODE_OPTS}"
+# The following applies to multiple commands (fs, dfs, fsck, distcp etc)
+export HADOOP_CLIENT_OPTS="-Xmx4000m $HADOOP_CLIENT_OPTS"
+#HADOOP_JAVA_PLATFORM_OPTS="-XX:-UsePerfData $HADOOP_JAVA_PLATFORM_OPTS"
+
+# On secure datanodes, user to run the datanode as after dropping privileges
+export HADOOP_SECURE_DN_USER=${HADOOP_SECURE_DN_USER}
+
+# Where log files are stored.  $HADOOP_HOME/logs by default.
+export HADOOP_LOG_DIR=/var/log/dblog/hadoop
+
+# Where log files are stored in the secure data environment.
+export HADOOP_SECURE_DN_LOG_DIR=${HADOOP_LOG_DIR}/${HADOOP_HDFS_USER}
+
+# The directory where pid files are stored. /tmp by default.
+# NOTE: this should be set to a directory that can only be written to by 
+#       the user that will run the hadoop daemons.  Otherwise there is the
+#       potential for a symlink attack.
+export HADOOP_PID_DIR=${HADOOP_PID_DIR}
+export HADOOP_SECURE_DN_PID_DIR=${HADOOP_PID_DIR}
+
+# A string representing this instance of hadoop. $USER by default.
+export HADOOP_IDENT_STRING=$USER
